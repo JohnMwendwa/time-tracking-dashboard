88667f2fdbb4c2b50ff0bcc8d79cd9c5ee1ae576
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2024-04-04 13:49:58 +0300

    Dynamically update values

---
+++ app.js
@@ -0,0 +1,63 @@
+const activityBtns = document.querySelectorAll(".activity__period li");
+const currentTime = document.querySelectorAll(".card__hour");
+const previousTime = document.querySelectorAll(".card__duration");
+
+// Fetch data from JSON file
+const fetchData = async () => {
+  try {
+    const response = await fetch("data.json");
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Error fetching data: ", error);
+  }
+};
+
+// Update the UI with the fetched data
+const updateUI = async (idx) => {
+  const data = await fetchData();
+  if (!data) return;
+
+  let period;
+  let timeOfPeriod;
+  switch (idx) {
+    case 0:
+      period = "daily";
+      timeOfPeriod = "Day";
+      break;
+    case 1:
+      period = "weekly";
+      timeOfPeriod = "Week";
+      break;
+    case 2:
+      period = "monthly";
+      timeOfPeriod = "Month";
+      break;
+    default:
+      period = "weekly";
+      timeOfPeriod = "Week";
+  }
+
+  //   Add active class to the selected period
+  activityBtns[idx].classList.add("active");
+
+  // Update the current time
+  for (let i = 0; i < data.length; i++) {
+    currentTime[i].textContent = `${data[i].timeframes[period].current}hrs`;
+
+    previousTime[
+      i
+    ].textContent = `Last ${timeOfPeriod} - ${data[i].timeframes[period].previous} hrs`;
+  }
+};
+
+// Event listener for activity buttons
+activityBtns.forEach((btn, idx) => {
+  btn.addEventListener("click", () => {
+    activityBtns.forEach((btn) => btn.classList.remove("active"));
+    updateUI(idx);
+  });
+});
+
+// Initial call to update the UI
+updateUI(1);
